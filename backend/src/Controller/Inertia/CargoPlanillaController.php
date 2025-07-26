013c2bfb9743dee94c6022972a9cd5ada48ef520
ddeleon-servir <ddeleon@servirsa.com>
2025-07-26 16:46:02 -0600

    WIP

---
+++ backend/src/Controller/Inertia/CargoPlanillaController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Controller\Inertia;
+
+use App\Data\MockData;
+use Rompetomp\InertiaBundle\Architecture\InertiaInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CargoPlanillaController extends AbstractController
+{
+    public function __construct(private InertiaInterface $inertia)
+    {
+    }
+
+    #[Route('/cargo-planilla/{id?}', name: 'inertia_cargo_planilla')]
+    public function index(?int $id = null): Response
+    {
+        $data = MockData::getData();
+        return $this->inertia->render('CargoPlanilla/Index', [
+            'cargoPlanillaId' => $id,
+            'centrosCosto' => $data['centrosCosto'],
+            'ubicaciones' => $data['ubicaciones'],
+            'areas' => $data['areas'],
+            'contratos' => $data['contratos'],
+            'rubros' => $data['rubros'],
+            'tarifas' => $data['tarifas'],
+            'regiones' => $data['regiones'],
+        ]);
+    }
+
+    #[Route('/cargo-planilla', name: 'inertia_cargo_planilla_store', methods: ['POST'])]
+    public function store(Request $request): Response
+    {
+        $data = $request->all();
+        // Simulate saving data
+        $this->addFlash('success', 'Cargo de planilla guardado con éxito');
+        return $this->inertia->location($request->input('saveAndAdd') ? '/cargo-planilla' : '/');
+    }
+}
