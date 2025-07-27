68bb5b3c9a1aaac39119d9e3fe4ccb21b14e0ead
ddeleon-servir <ddeleon@servirsa.com>
2025-07-27 09:54:14 -0600

    WIP

--- assets/react/app.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-const App = () => <h1>React sin Inertia</h1>;
-createRoot(document.getElementById('root')).render(<App />);
